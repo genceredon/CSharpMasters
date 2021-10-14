dfd0b3205aa2eacb239a052c17b8623e06ae86bd
genceredon <gjgceredon@gmail.com>
2021-10-14 16:49:02 +0800

    Homework 1: Use of Contextual Keywords in C#

---
+++ HomeworkOne/AddOperation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeworkOne
+{
+    public partial class Program
+    {
+        /// <summary>
+        /// Adds the two numbers.
+        /// </summary>
+        /// <returns></returns>
+        public static void AddTwoNumbers()
+        {
+            Console.WriteLine("Inside AddTwoNumbers()");
+            
+            Console.Write("Type a number, and then press Enter: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Type another number, and then press Enter: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            var sum = num1 + num2;
+
+            Console.WriteLine($"Your result: {num1} + {num2} = {sum}");
+        }
+    }
+}
---
+++ HomeworkOne/Program.cs
@@ -0,0 +1,25 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Author : Gen Ceredon
+//Created On : 10/13/2021
+//Description : CSharpMasters Homework#1: Use of Contextual Keywords in C# 
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace HomeworkOne
+{
+    public partial class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inside Main Program");
+            Console.WriteLine("Calling AddTwoNumbers()");
+
+            AddTwoNumbers();
+        }
+    }
+}
