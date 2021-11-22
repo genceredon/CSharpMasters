2ee0a350525d0f34fdafc111c2f838b602085d8f
genceredon <gjgceredon@gmail.com>
2021-11-23 04:06:45 +0800

    Assignment 4 - use of Generics

---
+++ Homework4/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public decimal DailyRate { get; set; }
+        public decimal DaysWorked { get; set; }
+        public decimal Salary { get; set; }
+        public decimal Commission { get; set; }
+        public decimal CommissionRate { get; set; }
+        public decimal Sales { get; set; }
+    }
+}
